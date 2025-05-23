2615b0f3a50e053f442907b60410709dbd4bbee3
DoggoMedic <josegonzalezag379@gmail.com>
2025-05-23 18:39:34 -0400

    Documentacion de Invitable e InvitadoExterno

--- src/main/java/org/udec/tarea2/Invitable.java
+++ src/main/java/org/udec/tarea2/Invitable.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 
 /**
  * Interfaz que define el comportamiento para los objetos que pueden ser invitados a reuniones.
- *
  * Los objetos que implementen esta interfaz deben definir los métodos para invitar a un individuo,
  * especificando la hora de la reunión, y para obtener el nombre completo del individuo invitado.
  */
--- src/main/java/org/udec/tarea2/InvitadoExterno.java
+++ src/main/java/org/udec/tarea2/InvitadoExterno.java
@@ -5,33 +5,60 @@ import java.util.Date;
 
 public class InvitadoExterno implements Invitable{
 
+    /** Variables que almacenan los datos del invitado.
+     */
     private String nombre, apellidos, correo;
-    private boolean yaInvitado = false;
 
+
+    /** Constructor de la clase InvitadoExterno, que inicializa los datos básicos de un invitado,
+     * incluyendo su nombre, apellidos y correo electrónico.
+     *
+     * @param nombre El nombre del invitado externo.
+     * @param apellidos Los apellidos del invitado externo.
+     * @param correo El correo electrónico asociado al invitado externo.
+     */
     public InvitadoExterno(String nombre, String apellidos, String correo){
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.correo = correo;
     }
 
+    /**
+     * Devuelve el nombre completo del invitado concatenando nombre y apellidos.
+     *
+     * @return Un string que representa el nombre completo del invitado.
+     */
     @Override
     public String getNombreCompleto() {
         return nombre + " " + apellidos;
     }
 
+    /**
+     * Obtiene la dirección de correo electrónico asociada al invitado externo.
+     *
+     * @return Un string del correo electrónico del invitado externo.
+     */
     public String getCorreo() {
         return correo;
     }
 
-    public boolean isInvitado() {
-        return yaInvitado;
-    }
-
+    /** Método que simula la invitación a un invitado externo a una reunión en un horario específico.
+     * Muestra un mensaje indicando que el invitado ha sido invitado junto con el horario de la reunión.
+     *
+     * @param hora El {@link Instant} que representa el horario de la reunión.
+     */
     @Override
     public void invitar(Instant hora) {
         System.out.println("Se ha invitado a externo " + getNombreCompleto() + " a reunión citada en horario: " + Reunion.SDF.format(Date.from(hora)));
     }
 
+    /**
+     * Devuelve una representación en string del objeto InvitadoExterno.
+     * Incluye el nombre completo y el correo electrónico asociado.
+     *
+     * @return Un string con la información en el formato:
+     * "Invitado externo [Nombre Completo] con el correo electrónico: [Correo].".
+     */
     @Override
     public String toString() {
         return "Invitado externo " + getNombreCompleto() + " con el correo electrónico: " + getCorreo() + ".";
