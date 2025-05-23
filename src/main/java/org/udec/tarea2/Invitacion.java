0e26f3bb8dd9538fac1e0a80a68586d8588e229a
DoggoMedic <josegonzalezag379@gmail.com>
2025-05-23 18:36:30 -0400

    Documentacion de Invitacion y Nota

--- src/main/java/org/udec/tarea2/Invitacion.java
+++ src/main/java/org/udec/tarea2/Invitacion.java
@@ -3,16 +3,37 @@ package org.udec.tarea2;
 import java.time.Instant;
 
 
+/** Clase que representa una invitación a una reunión específica,
+ * asignando un horario a un individuo que será invitado.
+ */
 public class Invitacion {
+
+    /** Representa el instante en el que comienza la reunión a la que se invita.
+     */
     private final Instant horaInicio;
+
+    /** Representa al individuo invitado a una reunión que implementa la interfaz {@link Invitable}.
+     */
     private final Invitable invitado;
 
+    /**
+     * Clase que representa una invitación a una reunión específica,
+     * asignando a un invitado un horario para asistir.
+     *
+     * @param invitado Individuo que implementa la interfaz {@link Invitable} al que se le quiere invitar.
+     * @param reunion Reunión a la que se le está invitando.
+     */
     public Invitacion(Invitable invitado, Reunion reunion){
         horaInicio = reunion.getHoraPrevista();
         this.invitado = invitado;
         this.invitado.invitar(horaInicio);
     }
 
+    /**
+     * Método para obtener el invitado asociado a la invitación.
+     *
+     * @return El invitado que implementa la interfaz {@link Invitable} y que fue invitado a la reunión.
+     */
     public Invitable getInvitado() {
         return invitado;
     }
--- src/main/java/org/udec/tarea2/Nota.java
+++ src/main/java/org/udec/tarea2/Nota.java
@@ -3,29 +3,65 @@ package org.udec.tarea2;
 import java.time.Instant;
 import java.util.Date;
 
+/**
+ * Clase que representa una nota con un contenido específico y un instante de tiempo de creación.
+ * Una nota se puede comparar con otra basándose en el momento que se creó.
+ */
 public class Nota implements Comparable<Nota> {
+    /** Contenido textual de la nota.
+     */
     private String contenido;
+
+    /** Variable que representa el instante exacto en el que la nota fue creada.
+     */
     private Instant horaDeCreacion;
 
+    /**
+     * Constructor que crea una {@link Nota} con su contenido y la hora de creación.
+     *
+     * @param contenido El contenido textual de la nota.
+     * @param horaDeCreacion El instante exacto en el que se creó la nota.
+     */
     public Nota(String contenido, Instant horaDeCreacion) {
         this.contenido = contenido;
         this.horaDeCreacion = horaDeCreacion;
     }
 
+    /** Devuelve una representación en string de la nota.
+     * La representación incluye la hora en la que fue creada y el contenido textual de la nota.
+     *
+     * @return Un string que describe la nota con su hora de creación y contenido.
+     */
     @Override
     public String toString() {
-
         return "Nota creada a las " + Reunion.SDF_HORA.format(Date.from(horaDeCreacion)) + ": " + contenido + ".";
     }
 
+    /** Obtiene el instante exacto en el que fue creada la nota.
+     *
+     * @return El instante de creación de la nota.
+     */
     public Instant getHoraDeCreacion() {
         return horaDeCreacion;
     }
 
+    /**
+     * Método que proporciona el contenido textual de la nota.
+     *
+     * @return El contenido de la nota en un string.
+     */
     public String getContenido() {
         return contenido;
     }
 
+    /**
+     * Compara esta instancia de {@link Nota} con otra instancia proporcionada, basándose en el instante de creación.
+     *
+     * @param nota2 La {@link Nota} con la cual se desea comparar esta instancia.
+     * @return -1 si esta nota fue creada antes que la nota proporcionada;
+     *          0 si ambas notas fueron creadas al mismo tiempo;
+     *          1 si esta nota fue creada después que la nota proporcionada.
+     */
     @Override
     public int compareTo(Nota nota2) {
         if(this.getHoraDeCreacion().isBefore(nota2.getHoraDeCreacion())){
